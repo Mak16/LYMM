de6ee908d38a0838c0fa1e831459af6acb942110
makeda banza <makedabanza16@gmail.com>
2024-02-23 21:27:57 +0200

    add services to Offres

--- src/components/Offres.jsx
+++ src/components/Offres.jsx
@@ -1,11 +1,17 @@
+import Services from "./Services";
+
 const Offres = () => {
   return (
     <div>
       <div
         className={`bg-[url(/src/assets/images/offers-bg.jpg)] w-full h-[40vh] sm:h-[90vh] bg-cover bg-center`}
       >
-        <div className="bg-black w-[90%] sm:h-[70vh] h-[30vh] m-auto opacity-75 relative sm:bottom-20 bottom-8 rounded-t-3xl ">
-          <h2 className="text-white flex justify-self-center">Nos offres</h2>
+        <div className="bg-black w-[90%] sm:h-[70vh] h-[35vh] flex items-center flex-col m-auto opacity-75 relative sm:bottom-20 bottom-8 rounded-t-3xl ">
+          <h2 className="text-white flex justify-self-center text-3xl mt-2">Nos offres</h2>
+          <Services classServices="text-white mt-2" title="Mise en location" >
+
+          </Services>
+          <span className="bg-white h-10 w-full "></span>
         </div>
       </div>
     </div>
---
+++ src/components/Services.jsx
@@ -0,0 +1,33 @@
+
+const Services = (props) => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h2 className={props.classServices} >{props.title}</h2>
+            <div className="mt-">
+                <p className="text-white flex mt-1" >
+                    <svg className="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipSCheckOne0"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z"/><path stroke="#000" strokeLinecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#C69C59" d="M0 0h48v48H0z" mask="url(#ipSCheckOne0)"/></svg>
+                    Nous rédigeons et publions l'annonce
+                </p>
+                <p className="text-white flex mt-1" >
+                    <svg className="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipSCheckOne0"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z"/><path stroke="#000" strokeLinecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#C69C59" d="M0 0h48v48H0z" mask="url(#ipSCheckOne0)"/></svg>
+                    Réalisons les visites
+                </p>
+                <p className="text-white flex mt-1" >
+                    <svg className="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipSCheckOne0"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z"/><path stroke="#000" strokeLinecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#C69C59" d="M0 0h48v48H0z" mask="url(#ipSCheckOne0)"/></svg>
+                    Vérifions les dossiers
+                </p>
+                <p className="text-white flex mt-1" >
+                    <svg className="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipSCheckOne0"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z"/><path stroke="#000" strokeLinecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#C69C59" d="M0 0h48v48H0z" mask="url(#ipSCheckOne0)"/></svg>
+                    Rédigeons le bail
+                </p>
+                <p className="text-white flex mt-1" >
+                    <svg className="mt-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipSCheckOne0"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z"/><path stroke="#000" strokeLinecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#C69C59" d="M0 0h48v48H0z" mask="url(#ipSCheckOne0)"/></svg>
+                    Effectuons l'état des lieux
+                </p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Services;
