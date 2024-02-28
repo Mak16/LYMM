fa08449409d7b6b7bd52e7ee910f36029bcd8a60
makeda banza <makedabanza16@gmail.com>
2024-02-28 12:50:50 +0200

    update compenent Offres

--- src/components/Offres.jsx
+++ src/components/Offres.jsx
@@ -6,14 +6,14 @@ const Offres = () => {
       <div
         className={`bg-[url(/src/assets/images/offers-bg.jpg)] w-full h-[40vh] sm:h-[90vh] bg-cover bg-center`}
       >
-        <div className="w-[90%] flex flex-col  relative m-auto ">
-        <div className="bg-black w-full sm:h-[70vh] h-[35vh] flex items-center flex-col  opacity-75  sm:bottom-20 bottom-8 rounded-t-3xl ">
+        <div className="w-[90%] flex flex-col  relative m-auto sm:bottom-20 bottom-8  ">
+        <div className="bg-black w-full sm:h-[70vh] h-[35vh] flex items-center flex-col  opacity-75  rounded-t-3xl ">
           <h2 className="text-white flex justify-self-center text-3xl mt-2">Nos offres</h2>
           <Services classServices="text-white mt-2" title="Mise en location" >
 
           </Services>
         </div>
-        <div className="bg-white h-10 w-full mt-"></div>
+        <div className="bg-white h-10 md:h-14 w-full "></div>
         </div>
       </div>
     </div>
